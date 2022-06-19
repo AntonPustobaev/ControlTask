297abcdd6b3333930e939bdbcecb1f48d47b84b7
Antonio <don_ne@mail.ru>
2022-06-19 16:05:36 +0400

    Final

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,1 +1,30 @@
-﻿
+﻿using System;
+Console.Clear();
+
+string[] array1 = new string[5] {"конь", "123", "Grib", "-2", "5LA"};
+Console.WriteLine(String.Join(" ", array1));
+string[] array2 = new string[array1.Length];
+
+FindSecondArray(array1, array2);
+PrintArray(array2);
+
+void FindSecondArray(string[] array1, string[] array2)
+{
+    int k = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[k] = array1[i];
+        k++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
